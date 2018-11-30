7e08ef02d744c2b59b049d071bb1fba950539e52
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-11-30 14:26:52 +0530

    Expose as library

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
+    library: 'Banana',
+    libraryExport: 'default',
     filename: 'banana-i18n.js',
     path: path.resolve(__dirname, 'dist')
   },
