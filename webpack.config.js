178f5778a9f678e6d9763ee0b09588a9c689ac75
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-12-07 10:31:56 +0530

    Set libraryTarget as umd

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     library: 'Banana',
     libraryExport: 'default',
+    libraryTarget: 'umd',
     filename: 'banana-i18n.js',
     path: path.resolve(__dirname, 'dist')
   },
